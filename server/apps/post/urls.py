ffef18c4631d60f81f86c15c2b66140ec0d8cec9
sk100 <sk100@korea.ac.kr>
2023-08-08 15:54:36 +0900

    [feat] detailPage 초기 views.py, urls.py, detail.html

--- server/apps/post/urls.py
+++ server/apps/post/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('',view_post_main,name='post_main'),
+    path('posts/<int:pk>',view_post_detail,name='post_detail'),
 ]
 
 if settings.DEBUG:
--- server/apps/post/views.py
+++ server/apps/post/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from apps.post.models import *
+from apps.comment.models import PostComment,StepComment
 
 # Create your views here.
 
 def view_post_main(requests):
     return render(requests, "post/main.html")
+
+def view_post_detail(requests,pk):
+
+    post=Post.objects.get(id=pk)
+    paths=Path.objects.filter(post=post).order_by("order")
+    for path in paths:
+        path.steps=Step.objects.filter(path=path).order_by("order") #?
+    post_comments=PostComment.objects.filter(post=post)
+    ctx={"post":post,"paths":paths,"post_comments":post_comments}
+
+    return render(requests,"post/detail.html",context=ctx)
