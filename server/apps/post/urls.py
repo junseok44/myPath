ef4d768e760755bba53e1971e464714e2ae6ada6
sk100 <sk100@korea.ac.kr>
2023-08-09 20:49:56 +0900

    [feat] step comment ajax로 추가

--- server/apps/post/urls.py
+++ server/apps/post/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('',view_post_main,name='post_main'),
     path('post/list', view_post_list, name="post_list"),
     path('post/<uuid:pk>',view_post_detail,name='post_detail'),
-    path('post/<uuid:pk>/comment_create',view_post_comment_create,name='post_comment_create'),
     path('post/step',view_step_detail_ajax,name='step_detail'),
-    path('post/step/create_comment',view_step_comment_create_ajax,name='step_comment_create'),
+    path('post/step/create_comment',view_step_create_comment_ajax,name='step_create_comment'),
+    path('post/<uuid:pk>/create_comment',view_post_create_comment,name='post_create_comment'),
     path('api/toggleBookMark/', toggle_bookmark_ajax),
     path('api/toggleLike/', toggle_like_ajax)   ,
 
--- server/apps/post/views.py
+++ server/apps/post/views.py
@@ -35,7 +35,7 @@ def view_post_detail(requests,pk):
 
     return render(requests,"post/detail.html",context=ctx)
 
-def view_post_comment_create(request,pk):
+def view_post_create_comment(request,pk):
     if request.method=="POST":
         comment = PostComment() 
         comment.post=Post.objects.get(id=pk)
@@ -51,19 +51,7 @@ from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.serializers import serialize
 
-# @csrf_exempt
-# def view_step_detail_ajax(request):
-#     req=json.loads(request.body)
-#     step_id=req['id']
-#     step=get_object_or_404(Step, pk=step_id)
-#     step_comments=StepComment.objects.filter(step=step)
-#     step_json=serialize('json',step)
-#     step_comments_json=serialize('json',step_comments)
-#     step_json=serialize('json',step)
-#     step_comments_json=serialize('json',step_comments_json)
-#     ctx={"step":step_json,"step_comments":step_comments_json}
 
-#     return JsonResponse(ctx)
 @csrf_exempt
 def view_step_detail_ajax(request):
     req = json.loads(request.body)
@@ -81,21 +69,19 @@ def view_step_detail_ajax(request):
         return JsonResponse(ctx)
     else:
         return HttpResponse('Failed: Post requests only.')
-
+    
 @csrf_exempt
-def view_step_comment_create_ajax(request):
+def view_step_create_comment_ajax(request):
     req=json.loads(request.body)
     step_id=req['step_id']
     text=req['text']
     if request.method=="POST":
-        step=Step.objects.get(id=step_id)
         comment=StepComment.objects.create(
             writer=request.user,
-            step=step,
+            step=get_object_or_404(Step, pk=step_id),
             text=text,
         )
-        # comment.save()
-        ctx={'id':step_id,'comment_id':comment.id,'text':text}
+        ctx={'step_id':step_id,'comment_id':comment.id,'text':text}
         return JsonResponse(ctx)
     else:
         return HttpResponse('Failed: Post requests only.')
