567385a3697387bbaabd56554e2f5056962e63af
sk100 <sk100@korea.ac.kr>
2023-08-09 17:55:52 +0900

    [feat] modal ajax(url get 오류 수정필요)

--- server/apps/post/views.py
+++ server/apps/post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from apps.post.models import *
 from apps.comment.models import PostComment,StepComment
 
@@ -23,22 +23,48 @@ def view_post_detail(requests,pk):
 
     return render(requests,"post/detail.html",context=ctx)
 
+def view_post_comment_create(request,pk):
+    if request.method=="POST":
+        comment = PostComment() 
+        comment.post=Post.objects.get(id=pk)
+        comment.text = request.POST['comment']
+        comment.save()
+        
+    return redirect(f'/post/{pk}/')  
+
+
+
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers import serialize
 
 @csrf_exempt
 def view_step_detail_ajax(request):
     req=json.loads(request.body)
     step_id=req['id']
-    step=Step.objects.get(id=step_id)
+    step=get_object_or_404(Step, pk=step_id)
+    step_comments=StepComment.objects.filter(step=step)
+    step_json=serialize('json',step)
+    step_comments_json=serialize('json',step_comments)
+    step_json=serialize('json',step)
+    step_comments_json=serialize('json',step_comments_json)
+    ctx={"step":step_json,"step_comments":step_comments_json}
 
-    return JsonResponse()
+    return JsonResponse(ctx)
 
 @csrf_exempt
 def view_step_comment_create_ajax(request):
     req=json.loads(request.body)
     step_id=req['id']
-    step=Step.objects.get(id=step_id)
+    content=req['content']
+    if request.method=="POST":
+        step=Step.objects.get(id=step_id)
+        comment=StepComment.objects.create(
+            step=step,
+            text=content,
+        )
+        # comment.save()
+    ctx={'id':step_id,'comment_id':comment.id,'content':content}
+    return JsonResponse(ctx)
     
-    return JsonResponse()
