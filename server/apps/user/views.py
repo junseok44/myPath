39e94a2135853a3b0ae8475a6441a7f5d381e467
Doeun <doeun990802@gmail.com>
2023-08-09 20:07:04 +0900

    [fix] post 필터링 수정

--- server/apps/user/views.py
+++ server/apps/user/views.py
@@ -9,17 +9,19 @@ def view_user_main(requests):
     return render(requests, "user/main.html")
 
 def my_page(requests):
-    my_posts = Post.objects.all()
-    bookmark_posts = BookMarkTable.objects.all()
-    like_posts = LikeTable.objects.all()
+    user_id = requests.user.id
+    my_posts = Post.objects.filter(user=user_id).values()
+    my_likes = LikeTable.objects.filter(user=user_id).select_related('post')
+    my_bookmarks = BookMarkTable.objects.filter(user=user_id).select_related('post')
 
-    ctx = {'my_posts': my_posts, 'bookmark_posts': bookmark_posts, 'like_posts': like_posts}
+    ctx = {'my_posts': my_posts, 'my_likes': my_likes, 'my_bookmarks': my_bookmarks}
     return render(requests, "user/my_page.html", ctx)
 
 def user_page(requests, id):
     user = User.objects.get(id=id)
-    posts = Post.objects.all()
+    posts = Post.objects.filter(user=user.id).values()
+    posts_count = Post.objects.filter(user=user.id).count()
 
-    ctx = {'user': user, 'posts': posts, "id": id} 
+    ctx = {'user': user, 'posts': posts, 'posts_count': posts_count, "id": id} 
     return render(requests, "user/user_page.html", ctx)
 
