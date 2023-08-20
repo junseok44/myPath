9fca97783a00d79d4b1e9666f390c57ac848f899
junseok <newtonjjang@naver.com>
2023-08-20 16:33:02 +0900

    allowed host, dockerignore .env 추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -35,8 +35,8 @@ AUTH_USER_MODEL = 'user.User'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["pathpiro-env.eba-bai3qfv2.ap-northeast-2.elasticbeanstalk.com", "localhost","127.0.0.1",
-                 "mypath.ap-northeast-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["eb.my-path.xyz", "localhost","127.0.0.1",
+                 "my-path.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition
