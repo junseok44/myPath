dd9e7ec1cdca158029eea0eedce1ea77eab09096
junseok <newtonjjang@naver.com>
2023-08-21 19:41:09 +0900

    [fix] csrf_trusted_origins 추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = ["hi.my-path.xyz", "localhost","127.0.0.1",
                  "mypath2.ap-northeast-2.elasticbeanstalk.com",
                  "mypath3.ap-northeast-2.elasticbeanstalk.com"]
 
-CSRF_TRUSTED_ORIGINS = ["https://eb.my-path.xyz"]
+CSRF_TRUSTED_ORIGINS = ["https://hi.my-path.xyz","http://mypath3.ap-northeast-2.elasticbeanstalk.com"]
 
 # Application definition
 
