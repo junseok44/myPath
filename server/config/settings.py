17a7537aa6ff4c5a2d47300b4a66a2b2e4cf2654
junseok <newtonjjang@naver.com>
2023-08-23 23:54:06 +0900

    [deploy] allowed host private ip 추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -40,6 +40,38 @@ ALLOWED_HOSTS = ["hi.my-path.xyz", "localhost","127.0.0.1",
 
 CSRF_TRUSTED_ORIGINS = ["https://hi.my-path.xyz","http://mypath-deploy.ap-northeast-2.elasticbeanstalk.com"]
 
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+        
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
