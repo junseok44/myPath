32be5b80047fa6e1d3eafd34045f5be8901ac125
junseok <newtonjjang@naver.com>
2023-09-15 00:13:31 +0900

    [fix] post migration 007->005로 돌리고 006 추가

--- server/config/settings.py
+++ server/config/settings.py
@@ -98,16 +98,16 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': env.db_url()
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 # }
+
+DATABASES = {
+    'default': env.db_url()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
