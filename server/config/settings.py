a2a8c89d3192a5c392e98e099b01e52ca074e981
junseok <newtonjjang@naver.com>
2023-09-25 15:16:56 +0900

    [fix] 헤더에 rooms 없애주기

--- server/config/settings.py
+++ server/config/settings.py
@@ -33,7 +33,7 @@ AUTH_USER_MODEL = 'user.User'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["hi.my-path.xyz", "localhost","127.0.0.1",
                  "mypath-deploy.ap-northeast-2.elasticbeanstalk.com"]
